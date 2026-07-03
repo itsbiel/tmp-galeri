530905469f3c7c485830972a0ce9802b2891e348
Gab <ktt.gabadrianputra1989@gmail.com>
2026-07-03 14:48:01 +0700

    Add files via upload

---
+++ asset/js/biel-dkfj2ijiwsjdsa.js
@@ -0,0 +1,203 @@
+const pageData = [
+  {
+    name: "KOITOTO_V1",
+    type: "Landing Page",
+    niche: "Togel",
+    thema: "profil",
+    url: "lp/formula1.html",
+    date: "2026-04-10" // New field
+  },
+  {
+    name: "Yu Gi OH",
+    type: "AMP",
+    niche: "AMP",
+    thema: "AMP",
+    url: "amp/yugioh.html",
+    date: "2026-04-10" // New field
+  },
+  {
+    name: "Simple AMP",
+    type: "AMP",
+    niche: "AMP",
+    thema: "AMP",
+    url: "amp/techno2.html",
+    date: "2026-04-10" // New field
+  },
+  {
+    name: "E-commerce Red Black",
+    type: "Landing Page",
+    niche: "Togel",
+    thema: "e-commerce",
+    url: "lp/ecom.html",
+    date: "2026-04-10" // New field
+  },
+  {
+    name: "Gundam",
+    type: "Landing Page",
+    niche: "Togel",
+    thema: "e-commerce",
+    url: "lp/gundam.html",
+    date: "2026-04-10" // New field
+  },
+  {
+    name: "Circle Logo",
+    type: "Landing Page",
+    niche: "Togel",
+    thema: "e-commerce",
+    url: "lp/ecom2.html",
+    date: "2026-04-10" // New field
+  },
+  {
+    name: "Slots",
+    type: "Landing Page",
+    niche: "Slot",
+    thema: "e-commerce",
+    url: "lp/slot.html",
+    date: "2026-04-10" // New field
+  },
+  {
+    name: "Slots 2",
+    type: "Landing Page",
+    niche: "Slot",
+    thema: "e-commerce",
+    url: "lp/slot2.html",
+    date: "2026-04-10" // New field
+  },
+  {
+    name: "Slots 3",
+    type: "Landing Page",
+    niche: "Slot",
+    thema: "e-commerce",
+    url: "lp/slot3.html",
+    date: "2026-04-10" // New field
+  },
+  {
+    name: "Windows 98",
+    type: "Landing Page",
+    niche: "Slot",
+    thema: "e-commerce",
+    url: "lp/koiwin.html",
+    date: "2026-04-12" // New field
+  },
+  {
+    name: "Slot 4",
+    type: "Landing Page",
+    niche: "Slot",
+    thema: "e-commerce",
+    url: "lp/slot4.html",
+    date: "2026-04-10" // New field
+  },
+  {
+    name: "Toto Rev 2",
+    type: "Landing Page",
+    niche: "Togel",
+    thema: "e-commerce",
+    url: "lp/toto2.html",
+    date: "2026-04-12" // New field
+  },
+  {
+    name: "Green TOTO",
+    type: "Landing Page",
+    niche: "Togel",
+    thema: "e-commerce",
+    url: "lp/toto3.html",
+    date: "2026-04-18" // New field
+  },
+  {
+    name: "Blogs Togel",
+    type: "Landing Page",
+    niche: "Togel",
+    thema: "Blog",
+    url: "lp/toto4.html",
+    date: "2026-04-18" // New field
+  },
+  {
+    name: "Argos.co.uk",
+    type: "Landing Page",
+    niche: "Togel",
+    thema: "e-commerce",
+    url: "lp/toto7.html",
+    date: "2026-04-19" // New field
+  },
+  {
+    name: "komputermedan.com",
+    type: "Landing Page",
+    niche: "Togel",
+    thema: "e-commerce",
+    url: "lp/toto8.html",
+    date: "2026-04-20" // New field
+  },
+  {
+    name: "startech.com.bd",
+    type: "Landing Page",
+    niche: "Togel",
+    thema: "e-commerce",
+    url: "lp/toto9.html",
+    date: "2026-04-22" // New field
+  },
+  {
+    name: "Dark Shining",
+    type: "AMP",
+    niche: "AMP",
+    thema: "AMP",
+    url: "amp/dark.html",
+    date: "2026-04-22" // New field
+  },
+  {
+    name: "decathlon.com.kh",
+    type: "Landing Page",
+    niche: "Togel",
+    thema: "e-commerce",
+    url: "lp/toto11.html",
+    date: "2026-04-23" // New field
+  },
+  {
+    name: "Redanje",
+    type: "AMP",
+    niche: "AMP",
+    thema: "AMP",
+    url: "amp/redanje.html",
+    date: "2026-04-24" // New field
+  },
+  {
+    name: "Responsive AMP",
+    type: "AMP",
+    niche: "AMP",
+    thema: "AMP",
+    url: "amp/responsive-amp.html",
+    date: "2026-04-25" // New field
+  },
+  {
+    name: "Slot APK",
+    type: "Landing Page",
+    niche: "Slot",
+    thema: "e-commerce",
+    url: "lp/slot5.html",
+    date: "2026-04-25" // New field
+  },
+  {
+    name: "Argo 2",
+    type: "Landing Page",
+    niche: "Slot",
+    thema: "e-commerce",
+    url: "lp/slot8.html",
+    date: "2026-05-10" // New field
+  },
+  {
+    name: "22-may",
+    type: "Landing Page",
+    niche: "Slot",
+    thema: "e-commerce",
+    url: "lp/22-may.html",
+    date: "2026-05-22" // New field
+  },
+  {
+    name: "Slot 9",
+    type: "Landing Page",
+    niche: "Slot",
+    thema: "e-commerce",
+    url: "lp/slot9.html",
+    date: "2026-05-22" // New field
+  }
+  
+];
